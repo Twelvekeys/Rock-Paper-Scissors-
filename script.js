7d49227272e3c12518417bcdd0f9ff5e29f70894
twelvekeys <idza2@yahoo.com>
2023-03-11 12:35:28 -0600

    added score and auto reset after 3 secs to site

--- script.js
+++ script.js
@@ -1,64 +1,70 @@
+
+let pScore = 0;
+let npcScore = 0;
 function npcchoice() {
     let npc = Math.floor(Math.random() * 3)
     if (npc === 0) return 'rock';
     else if (npc === 1) return 'paper';
     else return 'scissors';
 }
-
-function playerchoice(string) {
+//pc = gets playerchoice from button click in index file
+function playerchoice(pc) {
     let npc = npcchoice()
-     let pc = string;
-    if (pc === npc)
-        console.log('draw')
 
-    if (pc === 'rock') {
-        switch (npc) {
-            case 'paper':
-                console.log('you lost')
-                break;
-            case 'scissors':
-                console.log('you won')
-                break;
+    if (npcScore < 5 && pScore < 5) {
+        if (pc === npc) {
+            ++pScore
+             ++npcScore;
+        }
+        if (pc === 'rock') {
+            switch (npc) {
+                case 'paper':
+                     ++npcScore
+                    break;
+
+                case 'scissors':
+                    ++pScore
+                    break;
+            }
+        }
+        if (pc === 'paper') {
+            switch (npc) {
+                case 'scissors':
+                   ++npcScore
+                    break;
+
+                case 'rock':
+                    ++pScore
+                    break;
+            }
+        }
+        if (pc === 'scissors') {
+            switch (npc) {
+                case 'rock':
+                     ++npcScore
+                    break;
+
+                case 'paper':
+                     ++pScore
+                    break;
+            }
         }
     }
-    if (pc === 'paper') {
-        switch (npc) {
-            case 'scissors':
-                console.log('you lost')
-                break;
-            case 'rock':
-                console.log('you won')
-                break;
-        }
+    //code takes scores from int to string and displays it on webpage
+    document.getElementById("p1score").innerHTML = pScore.toString();
+    document.getElementById("npcscore").innerHTML = npcScore.toString();
+
+    //displays results of match and auto resets game after 3 secs
+    if (pScore === 5 && npcScore < 5) {
+        document.getElementById("display").innerHTML = "Congrats you've won";
+        setTimeout(() => { document.location.reload(); }, 3000);
     }
-    if (pc === 'scissors') {
-        switch (npc) {
-            case 'rock':
-                console.log('you lose')
-                break;
-            case 'paper':
-                console.log('you win')
-                break;
-        }
+    if (npcScore === 5 && pScore < 5) {
+        document.getElementById("display").innerHTML = 'Sorry Game Over';
+        setTimeout(() => { document.location.reload(); }, 3000);
     }
-
-
-
-
-    console.log("NPC chose"+'',npc)
-
+    if(npcScore === 5 && pScore === 5)  {
+        document.getElementById("display").innerHTML = 'Tied Game';
+        setTimeout(() => { document.location.reload(); }, 3000);
+    }
 }
-
-/*
-
-function playRound(playerSelection, computerSelection) {
-    // your code here!
-}
-
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
-if ( player === 0 && npc === 2) {
-
-}
-*/
